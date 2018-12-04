e64606dcccc4273f6270d2b71505d001585b5067
serinagg <sgarcia14@mail.csuchico.edu>
2018-12-03 22:39:52 -0800

    complete program with working student class

---
+++ student.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include "student.h"
+
+void student::setName(string firstName,string lastName)
+{
+        first = firstName;
+        last = lastName;
+}
+
+string student::fullName()
+{
+        string full;
+        full.append(first);
+
+        full.append(" ");
+        full.append(last);
+
+        return full;
+}
+
